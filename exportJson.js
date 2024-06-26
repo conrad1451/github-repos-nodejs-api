b7bc16cb2dd052140d039011f74cf3154af2a106
Conrad Hansen-Quartey <Chansenq@gmail.com>
2024-06-25 21:49:09 -0400

    Update exportJson.js

--- exportJson.js
+++ exportJson.js
@@ -1,14 +1,39 @@
-const fs = require("fs");
+// const fs = require("fs");
   
-// Read json file.
-//fs.readFile(".info.json", function(err, data) {
-fs.readFile("./info.json", function(err, data) {
-    // Check for the errors.
-    if (err) throw err;
+// // Read json file.
+// //fs.readFile(".info.json", function(err, data) {
+// fs.readFile("./info.json", function(err, data) {
+//     // Check for the errors.
+//     if (err) throw err;
   
-    // Converting to JSON.
-    const myInfo = JSON.parse(data);   
-    console.log(myInfo); // Print users 
+//     // Converting to JSON.
+//     const myInfo = JSON.parse(data);   
+//     console.log(myInfo); // Print users 
 
-  send(myInfo);
+//   send(myInfo);
+// });
+ 
+const express = require('express');
+const axios = require('axios');
+const app = express();
+const PORT = process.env.PORT || 8081;
+
+app.get('/', async (req, res) => {
+  const username = req.query.username || 'conrad1451';
+  try {
+    // const result = await axios.get(
+    //   `./info.json`
+    // );
+    const result = await axios.get(
+      "./info.json"
+    );
+  
+    res.send(result);
+  } catch (error) {
+    res.status(400).send('Error while getting the repository');
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
 });
