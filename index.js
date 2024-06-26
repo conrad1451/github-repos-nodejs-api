b8e10253f4021e5b69e164218e5cda86c5dcccfc
Conrad Hansen-Quartey <Chansenq@gmail.com>
2024-06-26 02:26:43 -0400

    Putting up a JSON object

--- index.js
+++ index.js
@@ -3,8 +3,14 @@ const app = express();
 
 const PORT = 3000;
 
+const dict = {"one" : [15, 4.5],
+        "two" : [34, 3.3],
+        "three" : [67, 5.0],
+        "four" : [32, 4.1]};
+
 app.get('/', (req, res) => {
-res.send("GET Request Called")
+  let dictstring = JSON.stringify(dict);
+  res.send(dictstring)
 })
 
 app.listen(PORT, function (err) {
