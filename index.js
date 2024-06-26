864c6475ebfea7da67a7ae8320dbbf502c30f4f1
Conrad Hansen-Quartey <Chansenq@gmail.com>
2024-06-26 03:34:15 -0400

    Fix error

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-// const fs = require("fs");
-  
-// // Read json file.
-// //fs.readFile(".info.json", function(err, data) {
-// fs.readFile("./info.json", function(err, data) {
-//     // Check for the errors.
-//     if (err) throw err;
-  
-//     // Converting to JSON.
-//     const myInfo = JSON.parse(data);   
-//     console.log(myInfo); // Print users 
-
-//   send(myInfo);
-// });
- 
 const express = require('express');
 const axios = require('axios');
 const app = express();
@@ -20,12 +5,9 @@ const PORT = process.env.PORT || 8081;
 
 app.get('/', async (req, res) => {
   const username = req.query.username || 'conrad1451';
-  try {
-    // const result = await axios.get(
-    //   `./info.json`
-    // );
+  try { 
     const result = await axios.get(
-      "./info.json"
+      `./info.json`
     );
   
     res.send(result);
